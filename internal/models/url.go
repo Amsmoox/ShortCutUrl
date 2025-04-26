6549cfc3d0afc19cc440d7cd26e380adbcf36cf7
Mh-Ayoub <ayoub@beewant.com>
2025-04-26 13:27:24 +0100

    [Models] define URL struct for database representation of shortened URLs

--- internal/models/url.go
+++ internal/models/url.go
@@ -1,1 +1,13 @@
- 
+package models
+
+import (
+	"time"
+)
+
+// URL represents the structure for a shortened URL in the database.
+type URL struct {
+	ID          int       `json:"id"`
+	OriginalURL string    `json:"original_url"`
+	ShortCode   string    `json:"short_code"`
+	CreatedAt   time.Time `json:"created_at"`
+} 
